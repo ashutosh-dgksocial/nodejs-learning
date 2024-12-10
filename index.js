4bbb2961a3d621ee8aa8391144f6f64d6ac444ef
ashutosh-dgksocial <ashutoshdgksocial@gmail.com>
2024-12-11 02:51:29 +0530

    http methods

--- index.js
+++ index.js
@@ -1,73 +1,75 @@
-const fs = require("node:fs");
-const process = require("process");
-const path = require("path");
+// const fs = require("node:fs");
+// const process = require("process");
+// const path = require("path");
 
-// for read file --------
-// readFile("./files/starter.txt", "utf8", (err, data) => {
-fs.readFile(
-  path.join(__dirname, "files", "text_append.txt"),
-  "utf8",
-  (err, data) => {
-    if (err) throw err;
-    console.log(data, `_Edited_by_Ask_`);
-    //   console.log(data.toString(), `._C`);
-  }
-);
-// for writefile ---------
-
-// fs.writeFile(
-//   path.join(__dirname, "files", "write.txt"),
-//   "hello this is Crazy Coder",
-//   (err) => {
-//     if (err) throw err;
-//     console.log("writefile is edited to new text");
-//   }
-// );
-console.log("hello... (-_-) you async bros");
-// for appending ---------
-// fs.appendFile(
+// // for read file --------
+// // readFile("./files/starter.txt", "utf8", (err, data) => {
+// fs.readFile(
 //   path.join(__dirname, "files", "text_append.txt"),
-//   "= I THink we're don't for appending today",
-//   (err) => {
+//   "utf8",
+//   (err, data) => {
 //     if (err) throw err;
-//     console.log("append is done");
+//     console.log(data, `_Edited_by_Ask_`);
+//     //   console.log(data.toString(), `._C`);
 //   }
 // );
+// // for writefile ---------
 
-// for write and append both --------
-// fs.writeFile(
-//   path.join(__dirname, "files", "text_append.txt"),
-//   " Nice to see you append ",
-//   (error) => {
-//     if (error) throw error;
-//     console.log("Write complete with mix write_append");
+// // fs.writeFile(
+// //   path.join(__dirname, "files", "write.txt"),
+// //   "hello this is Crazy Coder",
+// //   (err) => {
+// //     if (err) throw err;
+// //     console.log("writefile is edited to new text");
+// //   }
+// // );
+// console.log("hello... (-_-) you async bros");
+// // for appending ---------
+// // fs.appendFile(
+// //   path.join(__dirname, "files", "text_append.txt"),
+// //   "= I THink we're don't for appending today",
+// //   (err) => {
+// //     if (err) throw err;
+// //     console.log("append is done");
+// //   }
+// // );
 
-//     // fs.appendFile for appending inside write'
+// // for write and append both --------
+// // fs.writeFile(
+// //   path.join(__dirname, "files", "text_append.txt"),
+// //   " Nice to see you append ",
+// //   (error) => {
+// //     if (error) throw error;
+// //     console.log("Write complete with mix write_append");
 
-//     fs.appendFile(
-//       path.join(__dirname, "files", "text_append.txt"),
-//       " \n\nYes it is writty ",
-//       (err) => {
-//         if (err) throw err;
-//         console.log("appendFile complete");
+// //     // fs.appendFile for appending inside write'
 
-//         fs.rename(
-//           path.join(__dirname, "files", "text_append.txt"),
-//           path.join(__dirname, "files", "ashuRename.txt"),
-//           (err) => {
-//             if (err) throw err;
-//             console.log("renaming done");
-//           }
-//         );
-//       }
-//     );
-//   }
-// );
+// //     fs.appendFile(
+// //       path.join(__dirname, "files", "text_append.txt"),
+// //       " \n\nYes it is writty ",
+// //       (err) => {
+// //         if (err) throw err;
+// //         console.log("appendFile complete");
+
+// //         fs.rename(
+// //           path.join(__dirname, "files", "text_append.txt"),
+// //           path.join(__dirname, "files", "ashuRename.txt"),
+// //           (err) => {
+// //             if (err) throw err;
+// //             console.log("renaming done");
+// //           }
+// //         );
+// //       }
+// //     );
+// //   }
+// // );
 
 
 
 
-process.on("uncaughtException", (err) => {
-  console.error("There was an uncaught error", err);
-  process.exit(1);
-});
+// process.on("uncaughtException", (err) => {
+//   console.error("There was an uncaught error", err);
+//   process.exit(1);
+// });
+
+console.log('please fix your path and go to pokemon dir');
